726232692c199d7b9b40e238bc41a8fac3778fd0
Snower87 <55270945+Snower87@users.noreply.github.com>
2020-07-10 13:29:45 +0500

    3. Добавил класс BubbleSort, сортировка "методом пузырька".

--- src/main/java/ru/begletsov/javarush/BubbleSort.java
+++ src/main/java/ru/begletsov/javarush/BubbleSort.java
@@ -1,11 +1,37 @@
 package ru.begletsov.javarush;
 
+/**
+ * Class BubbleSort сортировка данных методом пузырька
+ * простая и неэффективная, сложность алгоритма O(n^2)
+ * 1) создание класса
+ * @author Sergei Begletsov
+ * @since 10.07.2020
+ * @version 1
+ */
+
 public class BubbleSort {
-    public static String test() {
-        return "test";
+    /**
+     * Метод меняет расположение элементов
+     * @param array значение ссылки на массив
+     * @param ind1 первый элемент
+     * @param ind2 второй элемент
+     */
+    public void swap(int[] array, int ind1, int ind2) {
+        int temp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = temp;
     }
 
-    public static void main(String[] args) {
-		BubbleSort.test();
+    BubbleSort(int[] array) {
+        boolean needIteration = true;
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i-1]) {
+                    swap(array, i, i - 1);
+                    needIteration = true;
+                }
+            }
+        }
     }
 }
--- src/test/java/ru/begletsov/javarush/BubbleSortTest.java
+++ src/test/java/ru/begletsov/javarush/BubbleSortTest.java
@@ -1,12 +1,22 @@
 package ru.begletsov.javarush;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class BubbleSortTest {
 
     @Test
-    public void test1() {
+    public void when10NumbersThenSorting() {
+        //1. Входные данные для сортировки
+        int[] numbers = {1, 5, 7, 8, 9, 2 ,4, 3, 6, 10};
+        BubbleSort bubbleSort = new BubbleSort(numbers);
+        //2. Выходные, ожидаемые данные
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //3. Выдать ошибку, если массивы не равны
+        assertArrayEquals(expected, numbers);
     }
 }
